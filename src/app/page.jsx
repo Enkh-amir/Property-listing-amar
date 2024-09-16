07ddc3b4d928fbdb85ce15efe3543abd1ef7e46c
Enkh-amar <greedyangir@gmail.com>
2024-09-16 22:20:01 +0800

    small changes on header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,52 @@
-import { MainLogo } from "@/app/components/MainLogo";
+import React from 'react';
+import { MainLogo } from '@/app/components/MainLogo';
+import { PhoneLogo } from '@/app/components/PhoneLogo';
 
-export default function Home() {
+async function fetchData() {
+  try {
+    const res = await fetch('https://dev.to/api/articles');
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return { error: 'Data fetching failed' };
+  }
+}
+
+export default async function Home() {
+  const data = await fetchData();
+
+  if (data.error) {
+    return <div>{data.error}</div>;
+  }
+
   return (
-    <div>
-      <header className="p-7">
+    <div className="flex flex-col items-center">
+      <header className="p-7 w-[1400px] flex justify-between ">
         <div className="flex gap-5">
           <div>
             <MainLogo />
           </div>
-          <div>
-            <label className="input input-bordered flex items-center gap-2 w-[600px] ">
-              <select className="select">
-                <option disabled selected>
-                  Rent
-                </option>
-                <option>Homer</option>
-                <option>Marge</option>
-                <option>Bart</option>
-                <option>Lisa</option>
-                <option>Maggie</option>
-              </select>
-              <input type="text" className="grow" placeholder="Search" />
+          <div className="flex">
+            <select className="select border-neutral-300 border-r-0 rounded-r-none focus:outline-none">
+              <option disabled selected>
+                Rent
+              </option>
+              <option>Homer</option>
+              <option>Marge</option>
+              <option>Bart</option>
+              <option>Lisa</option>
+              <option>Maggie</option>
+            </select>
+            <label className="input border-neutral-300 border-l-0 rounded-l-none flex items-center gap-2 w-[600px] focus-within:outline-none">
+              <input
+                type="text"
+                className="grow focus:outline-none border-none"
+                placeholder="Search"
+              />
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 16 16"
@@ -36,8 +62,20 @@ export default function Home() {
             </label>
           </div>
         </div>
+        <div>
+          <div className="flex justify-center items-center gap-1">
+            <PhoneLogo />
+            <div>+ 1 (800) 657 8976</div>
+          </div>
+        </div>
       </header>
-      <main></main>
+      <main>
+        <div>
+          {data.map((article, index) => (
+            <div key={index}>{article.title}</div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
